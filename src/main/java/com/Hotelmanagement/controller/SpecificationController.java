31305b18ac157b2f57c05afcbe35310c6753903b
unknown <cogjava3201@iiht.tech>
2022-10-20 17:36:54 +0530

    Dyanmic search

--- src/main/java/com/Hotelmanagement/controller/SpecificationController.java
+++ src/main/java/com/Hotelmanagement/controller/SpecificationController.java
@@ -1,14 +1,21 @@
 package com.Hotelmanagement.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Hotelmanagement.dto.SearchDto;
 import com.Hotelmanagement.entity.Address;
 import com.Hotelmanagement.entity.Hotel;
 import com.Hotelmanagement.repository.HotelRepository;
@@ -16,7 +23,7 @@ import com.Hotelmanagement.service.HotelManagementService;
 import com.Hotelmanagement.specification.HotelSpecification;
 
 @RestController
-@RequestMapping("/specification")
+@RequestMapping("/hotel/specification")
 public class SpecificationController {
 
 	@Autowired
@@ -24,11 +31,46 @@ public class SpecificationController {
 
 	@Autowired
 	private HotelRepository repository;
+	
+	@PostMapping("/filter")
+	public List<Hotel> filterHotels(@RequestBody List<SearchDto> dtos){
+		
+//		List<Specification<Hotel>> collect = dtos.stream().map(this::getSpecification).collect(Collectors.toList());
+		Specification<Hotel> specification = getSpecification(dtos);
+		
+		return repository.findAll(specification);
+	}
 
+	public Specification<Hotel> getSpecification(List<SearchDto> dtos){
+		List<Predicate> prdicates = new ArrayList<>();
+		
+		return (root, query, criteriaBuilder) -> {
+			for(SearchDto dto : dtos) {
+				if(dto.getJoinAttribute()!=null) {
+					Predicate join = criteriaBuilder.equal(root.join(dto.getJoinAttribute()).get(dto.getColumn()),dto.getValue() );
+					prdicates.add(join);
+				}else {
+				Predicate equal = criteriaBuilder.equal(root.get(dto.getColumn()), dto.getValue());
+				prdicates.add(equal);
+				}
+			}
+			return criteriaBuilder.and(prdicates.toArray(new Predicate[0]));
+			
+		};
+	
+	}
+	
 	@GetMapping("/search")
 	public List<Hotel> searchHotel(@RequestParam String name, @RequestParam Address address) {
 		Specification<Hotel> specification = Specification.where(
 				HotelSpecification.searchHotelbyName(name).and(HotelSpecification.searchHotelbyAddress(address)));
 		return repository.findAll(specification);
 	}
+
+	@GetMapping("/byname")
+	public List<Hotel> searchHotelbyName(@RequestParam String name) {
+		Specification<Hotel> specification = Specification.where(HotelSpecification.searchHotelbyName(name));
+		return repository.findAll(specification);
+	}
+
 }
---
+++ src/main/java/com/Hotelmanagement/dto/SearchDto.java
@@ -0,0 +1,33 @@
+package com.Hotelmanagement.dto;
+
+public class SearchDto {
+	
+	private String column;
+	private String value;
+	private String joinAttribute;
+
+	
+	public String getColumn() {
+		return column;
+	}
+	public void setColumn(String column) {
+		this.column = column;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getJoinAttribute() {
+		return joinAttribute;
+	}
+	public void setJoinAttribute(String joinAttribute) {
+		this.joinAttribute = joinAttribute;
+	}
+	
+	
+	
+	
+
+}
